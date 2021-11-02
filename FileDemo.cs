f403b6a210062c7c046e01310614c887c389ac52
mavinii <pg.marcosoliveira@icloud.com>
2021-11-02 17:56:24 +0000

    Add project files.

---
+++ FileDemo.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile002
+{
+    public class FileDemo
+    {
+        public static void ReadDemo(string file = "jamaica.txt")
+        {
+            string path = "C:/Users/pgmar/Downloads";
+
+            string fileToRead = $"{path}/{file}";
+
+            //  Console.WriteLine($"Reading File: {fileToRead}");
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileToRead))
+                {
+
+                    //  Console.WriteLine($"Starting to read {fileToRead}");
+
+                    string line;
+
+                    while ((line = sr.ReadLine()) is not null)
+                    {
+                        string[] splitData = line.Split(":");
+                        // Console.WriteLine($"{splitData[0]}\t\t\t{splitData[1]}");
+
+                        string heroName = String.Format("{0,-20}", splitData[0]);
+                        Console.WriteLine($"{heroName}\t{splitData[1]}");
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"The {fileToRead} file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            FileDemo.OutputSeparator();
+
+        }
+
+        public static void WriteDemo(string file, string[] heroes)
+        {
+
+            string path = "C:/Users/pgmar/Downloads";
+
+            string fileToWrite = $"{path}/{file}";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fileToWrite))
+                {
+                    foreach (string hero in heroes)
+                    {
+
+                        sw.WriteLine(hero);
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"The {fileToWrite} file could not be written");
+                Console.WriteLine(e.Message);
+
+            }
+
+        }
+
+
+        // https://docs.microsoft.com/en-us/dotnet/standard/base-types/padding
+        public static void OutputSeparator()
+        {
+            Console.WriteLine("=".PadRight(80, '='));
+        }
+
+        public static char[] SplitAsString(string stringToSplit)
+        {
+
+            char[] characters = stringToSplit.ToCharArray();
+
+            foreach (char c in characters)
+            {
+                Console.Write($" {c} ");
+
+            }
+            Console.WriteLine();
+
+
+            return characters;
+
+            // faster way is to just write:
+            //return stringToSplit.ToCharArray();
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ReadFile002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GitHub Link: https://github.com/DorsetOppSemster2021/ReadFile002
+            //https://docs.microsoft.com/en-us/dotnet/standard/base-types/padding
+
+            //FileDemo.ReadDemo("Jamaica.txt");
+
+            string dc_heroesFile = "heroes-dc.txt";
+            string[] dc_heroes = { "Batman:Bruce Wayne", "Superman:Clarke Kent", "Aquaman:Arthur Curry" };
+
+            FileDemo.WriteDemo(dc_heroesFile, dc_heroes);
+            FileDemo.ReadDemo(dc_heroesFile);
+
+            //FileDemo.OutputSeparator();
+
+            string marvel_heroesFile = "heroes-marvel.txt";
+            string[] marvel_heroes = { "SpiderMan:Peter Parker", "The Hulk:David Banner", "Captain America:Steve Rogers" };
+            FileDemo.WriteDemo(marvel_heroesFile, marvel_heroes);
+
+            FileDemo.ReadDemo(marvel_heroesFile);
+
+            char[] characters = FileDemo.SplitAsString("supercalifragilisticexpialidocious");
+
+            Console.WriteLine($"The character at index position 10, live position 11 is {characters[10]}");
+
+            //Console.WriteLine("Done");
+        }
+    }
+}
